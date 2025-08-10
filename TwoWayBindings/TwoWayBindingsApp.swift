ab7736b7cb743d5010f8d408ecf725cb07e5bfb9
Dan Simek <djsimek@yahoo.com>
2025-08-09 23:14:10 -0500

    Initial Commit

---
+++ TwoWayBindings/TwoWayBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingsApp.swift
+//  TwoWayBindings
+//
+//  Created by Dan Simek on 8/9/25.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
